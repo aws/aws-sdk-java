5cc56dcad4117db6ad38fb944b5bcc57c295ec60
Andreas Kohn <andreas.kohn@gmail.com>
2014-06-20 19:54:12 +0200

    Reset the request content in retry cases before wrapping and buffering it
    Content without known length (something that happens with requests for #setBucketLoggingConfiguration() for example)
    will get wrapped and buffered, before it eventually ends up as "entity". Marking/Resetting the content stream
    returned by BufferedHttpEntity is pretty pointless, as that is *recreated* on every call to #getContent(). At the same
    time when the BufferedHttpEntity is created the second time (inside #createHttpRequest()), it's already at its end,
    so the buffering produces an *empty* buffer/entity.
    
    This fixes cases where an S3 request with such an entity hits a 307 redirect.

--- src/main/java/com/amazonaws/http/AmazonHttpClient.java
+++ src/main/java/com/amazonaws/http/AmazonHttpClient.java
@@ -355,6 +355,20 @@ public class AmazonHttpClient {
                     requestLog.debug("Sending Request: " + request.toString());
                  }
 
+                InputStream content = request.getContent();
+                if ( content != null ) {
+                    if ( requestCount > 1 ) {   // retry
+                        if ( content.markSupported() ) {
+                            content.reset();
+                            content.mark(-1);
+                        }
+                    } else {
+                        if ( content.markSupported() ) {
+                            content.mark(-1);
+                        }
+                    }
+                }
+
                 httpRequest = httpRequestFactory.createHttpRequest(request, config, executionContext);
 
                 if (httpRequest instanceof HttpEntityEnclosingRequest) {
@@ -374,20 +388,6 @@ public class AmazonHttpClient {
                                              config.getRetryPolicy());
                     } finally {
                         awsRequestMetrics.endEvent(Field.RetryPauseTime);
-                    }
-                }
-
-                if ( entity != null ) {
-                    InputStream content = entity.getContent();
-                    if ( requestCount > 1 ) {   // retry
-                        if ( content.markSupported() ) {
-                            content.reset();
-                            content.mark(-1);
-                        }
-                    } else {
-                        if ( content.markSupported() ) {
-                            content.mark(-1);
-                        }
                     }
                 }
 
