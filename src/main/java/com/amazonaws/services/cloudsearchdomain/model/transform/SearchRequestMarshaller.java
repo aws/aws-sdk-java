345870dac6f5eccef0efb0f9baf6dcfb679bacc7
Geoff Sallee <geoff@netbrains.com>
2014-07-16 10:41:30 -0700

    Fixed a syntax error

--- src/main/java/com/amazonaws/services/cloudsearchdomain/model/transform/SearchRequestMarshaller.java
+++ src/main/java/com/amazonaws/services/cloudsearchdomain/model/transform/SearchRequestMarshaller.java
@@ -29,54 +29,56 @@ import java.io.ByteArrayInputStream;
  */
 public class SearchRequestMarshaller implements Marshaller<Request<SearchRequest>, SearchRequest> {
 
-    public Request<SearchRequest> marshall(SearchRequest searchRequest) {
-    if (searchRequest == null) {
-        throw new AmazonClientException("Invalid argument passed to marshall(...)");
-    }
+	public Request<SearchRequest> marshall(SearchRequest searchRequest) {
+		if (searchRequest == null) {
+			throw new AmazonClientException("Invalid argument passed to marshall(...)");
+		}
 
-        Request<SearchRequest> request = new DefaultRequest<SearchRequest>(searchRequest, "AmazonCloudSearchDomain");
-        String target = "AmazonCloudSearch2013.Search";
-        request.addHeader("X-Amz-Target", target);
+		Request<SearchRequest> request = new DefaultRequest<SearchRequest>(searchRequest, "AmazonCloudSearchDomain");
+		String target = "AmazonCloudSearch2013.Search";
+		request.addHeader("X-Amz-Target", target);
 
-        request.setHttpMethod(HttpMethodName.GET);
-        String uriResourcePath = "/2013-01-01/search?format=sdk&pretty=true&cursor={Cursor}&expr={Expr}&facet={Facet}&fq={FilterQuery}&highlight={Highlight}&partial={Partial}&q={Query}&q.options={QueryOptions}&q.parser={QueryParser}&return={Return}&size={Size}&sort={Sort}&start={Start}";
-        uriResourcePath = uriResourcePath.replace("{Cursor}", (searchRequest.getCursor() == null) ? "" : StringUtils.fromString(searchRequest.getCursor()));
-        uriResourcePath = uriResourcePath.replace("{Expr}", (searchRequest.getExpr() == null) ? "" : StringUtils.fromString(searchRequest.getExpr()));
-        uriResourcePath = uriResourcePath.replace("{Facet}", (searchRequest.getFacet() == null) ? "" : StringUtils.fromString(searchRequest.getFacet()));
-        uriResourcePath = uriResourcePath.replace("{FilterQuery}", (searchRequest.getFilterQuery() == null) ? "" : StringUtils.fromString(searchRequest.getFilterQuery()));
-        uriResourcePath = uriResourcePath.replace("{Highlight}", (searchRequest.getHighlight() == null) ? "" : StringUtils.fromString(searchRequest.getHighlight()));
-        uriResourcePath = uriResourcePath.replace("{Partial}", (searchRequest.isPartial() == null) ? "" : StringUtils.fromBoolean(searchRequest.isPartial()));
-        uriResourcePath = uriResourcePath.replace("{Query}", (searchRequest.getQuery() == null) ? "" : StringUtils.fromString(searchRequest.getQuery()));
-        uriResourcePath = uriResourcePath.replace("{QueryOptions}", (searchRequest.getQueryOptions() == null) ? "" : StringUtils.fromString(searchRequest.getQueryOptions()));
-        uriResourcePath = uriResourcePath.replace("{QueryParser}", (searchRequest.getQueryParser() == null) ? "" : StringUtils.fromString(searchRequest.getQueryParser()));
-        uriResourcePath = uriResourcePath.replace("{Return}", (searchRequest.getReturn() == null) ? "" : StringUtils.fromString(searchRequest.getReturn()));
-        uriResourcePath = uriResourcePath.replace("{Size}", (searchRequest.getSize() == null) ? "" : StringUtils.fromLong(searchRequest.getSize()));
-        uriResourcePath = uriResourcePath.replace("{Sort}", (searchRequest.getSort() == null) ? "" : StringUtils.fromString(searchRequest.getSort()));
-        uriResourcePath = uriResourcePath.replace("{Start}", (searchRequest.getStart() == null) ? "" : StringUtils.fromLong(searchRequest.getStart()));
+		request.setHttpMethod(HttpMethodName.GET);
+		String uriResourcePath = "/2013-01-01/search?format=sdk&pretty=true&cursor={Cursor}&expr={Expr}&facet={Facet}&fq={FilterQuery}&highlight={Highlight}&partial={Partial}&q={Query}&q.options={QueryOptions}&q.parser={QueryParser}&return={Return}&size={Size}&sort={Sort}&start={Start}";
+		uriResourcePath = uriResourcePath.replace("{Cursor}", (searchRequest.getCursor() == null) ? "" : StringUtils.fromString(searchRequest.getCursor()));
+		uriResourcePath = uriResourcePath.replace("{Expr}", (searchRequest.getExpr() == null) ? "" : StringUtils.fromString(searchRequest.getExpr()));
+		uriResourcePath = uriResourcePath.replace("{Facet}", (searchRequest.getFacet() == null) ? "" : StringUtils.fromString(searchRequest.getFacet()));
+		uriResourcePath = uriResourcePath.replace("{FilterQuery}", (searchRequest.getFilterQuery() == null) ? "" : StringUtils.fromString(searchRequest.getFilterQuery()));
+		uriResourcePath = uriResourcePath.replace("{Highlight}", (searchRequest.getHighlight() == null) ? "" : StringUtils.fromString(searchRequest.getHighlight()));
+		uriResourcePath = uriResourcePath.replace("{Partial}", (searchRequest.isPartial() == null) ? "" : StringUtils.fromBoolean(searchRequest.isPartial()));
+		uriResourcePath = uriResourcePath.replace("{Query}", (searchRequest.getQuery() == null) ? "" : StringUtils.fromString(searchRequest.getQuery()));
+		uriResourcePath = uriResourcePath.replace("{QueryOptions}", (searchRequest.getQueryOptions() == null) ? "" : StringUtils.fromString(searchRequest.getQueryOptions()));
+		uriResourcePath = uriResourcePath.replace("{QueryParser}", (searchRequest.getQueryParser() == null) ? "" : StringUtils.fromString(searchRequest.getQueryParser()));
+		uriResourcePath = uriResourcePath.replace("{Return}", (searchRequest.getReturn() == null) ? "" : StringUtils.fromString(searchRequest.getReturn()));
+		uriResourcePath = uriResourcePath.replace("{Size}", (searchRequest.getSize() == null) ? "" : StringUtils.fromLong(searchRequest.getSize()));
+		uriResourcePath = uriResourcePath.replace("{Sort}", (searchRequest.getSort() == null) ? "" : StringUtils.fromString(searchRequest.getSort()));
+		uriResourcePath = uriResourcePath.replace("{Start}", (searchRequest.getStart() == null) ? "" : StringUtils.fromLong(searchRequest.getStart()));
 
-        uriResourcePath = uriResourcePath.replaceAll("//", "/");
+		uriResourcePath = uriResourcePath.replaceAll("//", "/");
 
-        if (uriResourcePath.contains("?")) {
-            String queryString = uriResourcePath.substring(uriResourcePath.indexOf("?") + 1);
-            uriResourcePath    = uriResourcePath.substring(0, uriResourcePath.indexOf("?"));
+		if (uriResourcePath.contains("?")) {
+			String queryString = uriResourcePath.substring(uriResourcePath.indexOf("?") + 1);
+			uriResourcePath = uriResourcePath.substring(0, uriResourcePath.indexOf("?"));
 
-            for (String s : queryString.split("[;&]")) {
+			for (String s : queryString.split("[;&]")) {
 
-                String[] nameValuePair = s.split("=");
-                if (nameValuePair.length >= 2) {
-	                String key = nameValuePair[0], value = "";
-	                for (int i = 1; i < nameValuePair.length; i++) {
-		                value += nameValuePair[i];
-	                }
-                    if(!(value.isEmpty())) {
-                        request.addParameter(key, value);
-                }
-            }
-        }
-        request.setResourcePath(uriResourcePath);
+				String[] nameValuePair = s.split("=");
+				if (nameValuePair.length >= 2) {
+					String key = nameValuePair[0], value = "";
+					for (int i = 1; i < nameValuePair.length; i++) {
+						value += nameValuePair[i];
+					}
+					if (!(value.isEmpty())) {
+						request.addParameter(key, value);
+					}
+				}
+			}
+		}
+		request.setResourcePath(uriResourcePath);
 
-        request.setContent(new ByteArrayInputStream(new byte[0]));
+		request.setContent(new ByteArrayInputStream(new byte[0]));
 
-        return request;
-    }
+		return request;
+
+	}
 }
