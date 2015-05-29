19d2c2e99ad5f191c96b711769f4999afaa9d258
Jack Phelan <jackphel@gmail.com>
2015-05-28 18:25:17 -0700

    case insensitive metadata header parsing

--- aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/Headers.java
+++ aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/Headers.java
@@ -23,17 +23,17 @@ public interface Headers {
      * Standard HTTP Headers
      */
 
-    public static final String CACHE_CONTROL = "Cache-Control";
-    public static final String CONTENT_DISPOSITION = "Content-Disposition";
-    public static final String CONTENT_ENCODING = "Content-Encoding";
-    public static final String CONTENT_LENGTH = "Content-Length";
-    public static final String CONTENT_RANGE = "Content-Range";
-    public static final String CONTENT_MD5 = "Content-MD5";
-    public static final String CONTENT_TYPE = "Content-Type";
-    public static final String DATE = "Date";
-    public static final String ETAG = "ETag";
-    public static final String LAST_MODIFIED = "Last-Modified";
-    public static final String SERVER = "Server";
+    public static final String CACHE_CONTROL = "cache-control";
+    public static final String CONTENT_DISPOSITION = "content-disposition";
+    public static final String CONTENT_ENCODING = "content-encoding";
+    public static final String CONTENT_LENGTH = "content-length";
+    public static final String CONTENT_RANGE = "content-range";
+    public static final String CONTENT_MD5 = "content-md5";
+    public static final String CONTENT_TYPE = "content-type";
+    public static final String DATE = "date";
+    public static final String ETAG = "etag";
+    public static final String LAST_MODIFIED = "last-modified";
+    public static final String SERVER = "server";
 
     /*
      * Amazon HTTP Headers
