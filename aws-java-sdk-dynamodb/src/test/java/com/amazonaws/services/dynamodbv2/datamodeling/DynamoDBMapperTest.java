1069d4f0b8d30ff3a07ed505321ee0d2ded3a569
Paulo Miguel Almeida <paulo.ubuntu@gmail.com>
2016-01-19 16:16:25 -0200

    Add test not overriding table name

--- aws-java-sdk-dynamodb/src/test/java/com/amazonaws/services/dynamodbv2/datamodeling/DynamoDBMapperTest.java
+++ aws-java-sdk-dynamodb/src/test/java/com/amazonaws/services/dynamodbv2/datamodeling/DynamoDBMapperTest.java
@@ -8,7 +8,22 @@ import com.amazonaws.services.dynamodbv2.model.DeleteTableRequest;
 public class DynamoDBMapperTest {
 
     @Test
-    public void testGenerateDeleteTableRequest(){
+    public void testGenerateDeleteTableRequest1(){
+        final String tableName = "OBJECTORMEXAMPLE";
+
+        DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(null);
+
+        // Create the simplest table as possible for our test purposes only
+        @DynamoDBTable(tableName = tableName)
+        class ObjectORMExample{}
+
+        // test
+        DeleteTableRequest deleteTableRequest = dynamoDBMapper.generateDeleteTableRequest(ObjectORMExample.class);
+        assertEquals(deleteTableRequest.getTableName(), tableName);
+    }
+
+    @Test
+    public void testGenerateDeleteTableRequest2(){
         String prefix = "DEV-";
         final String tableName = "OBJECTORMEXAMPLE";
 
