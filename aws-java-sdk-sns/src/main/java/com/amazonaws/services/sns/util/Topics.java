9cd6a8c4161c82d90575605204e71cb28b9ffc27
Chi Hyun Lim <chidoli37@gmail.com>
2016-02-15 14:54:39 -0800

    Adds an optional parameter "extendPolicy"
    Adds an optional parameter "extendPolicy" to help extend existing policy.

--- aws-java-sdk-sns/src/main/java/com/amazonaws/services/sns/util/Topics.java
+++ aws-java-sdk-sns/src/main/java/com/amazonaws/services/sns/util/Topics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright 2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License").
  * You may not use this file except in compliance with the License.
@@ -109,12 +109,83 @@ public class Topics {
      */
     public static String subscribeQueue(AmazonSNS sns, AmazonSQS sqs, String snsTopicArn, String sqsQueueUrl)
                 throws AmazonClientException, AmazonServiceException {
+        return Topics.subscribeQueue(sns, sqs, snsTopicArn, sqsQueueUrl, false);
+    }
+
+    /**
+     * Subscribes an existing Amazon SQS queue to an existing Amazon SNS topic.
+     * <p>
+     * The specified Amazon SNS client will be used to send the subscription
+     * request, and the Amazon SQS client will be used to modify the policy on
+     * the queue to allow it to receive messages from the SNS topic.
+     * <p>
+     * The policy applied to the SQS queue is similar to this:
+     * <pre>
+     * {
+     *    "Version" : "2008-10-17",
+     *    "Statement" : [{
+     *       "Sid" : "topic-subscription-arn:aws:sns:us-west-2:599109622955:myTopic",
+     *       "Effect" : "Allow",
+     *       "Principal" : {
+     *          "AWS":["*"]
+     *       },
+     *       "Action" : ["sqs:SendMessage"],
+     *       "Resource":["arn:aws:sqs:us-west-2:599109622955:myQueue"],
+     *       "Condition" : {
+     *          "ArnLike":{
+     *             "aws:SourceArn":["arn:aws:sns:us-west-2:599109622955:myTopic"]
+     *          }
+     *       }
+     *    }]
+     * }
+     * </pre>
+     * <p>
+     * <b>IMPORTANT</b>: There might be a small time period immediately after
+     * subscribing the SQS queue to the SNS topic and updating the SQS queue's
+     * policy, where messages are not able to be delivered to the queue. After a
+     * moment, the new queue policy will propagate and the queue will be able to
+     * receive messages. This delay only occurs immediately after initially
+     * subscribing the queue.
+     * <p>
+     * <b>IMPORTANT</b>: The specified queue and topic (as well as the SNS and
+     * SQS client) should both be located in the same AWS region.
+     *
+     * @param sns
+     *            The Amazon SNS client to use when subscribing the queue to the
+     *            topic.
+     * @param sqs
+     *            The Amazon SQS client to use when applying the policy to allow
+     *            subscribing to the topic.
+     * @param snsTopicArn
+     *            The Amazon Resource Name (ARN) uniquely identifying the Amazon
+     *            SNS topic. This value is returned form Amazon SNS when
+     *            creating the topic.
+     * @param sqsQueueUrl
+     *            The URL uniquely identifying the Amazon SQS queue. This value
+     *            is returned from Amazon SQS when creating the queue.
+     * @param extendPolicy
+     *            Decides behavior to overwrite the existing policy or extend it.
+     *
+     * @throws AmazonClientException
+     *             If any internal errors are encountered inside the client
+     *             while attempting to make the request or handle the response.
+     *             For example if a network connection is not available.
+     * @throws AmazonServiceException
+     *             If an error response is returned by AmazonSNS indicating
+     *             either a problem with the data in the request, or a server
+     *             side issue.
+     *
+     * @return The subscription ARN as returned by Amazon SNS when the queue is
+     *         successfully subscribed to the topic.
+     */
+    public static String subscribeQueue(AmazonSNS sns, AmazonSQS sqs, String snsTopicArn, String sqsQueueUrl, boolean extendPolicy)
+                throws AmazonClientException, AmazonServiceException {
         List<String> sqsAttrNames = Arrays.asList(QueueAttributeName.QueueArn.toString(),
                                                   QueueAttributeName.Policy.toString());
         Map<String, String> sqsAttrs = sqs.getQueueAttributes(sqsQueueUrl, sqsAttrNames).getAttributes();
         String sqsQueueArn = sqsAttrs.get(QueueAttributeName.QueueArn.toString());
 
-        Policy policy = Policy.fromJson(sqsAttrs.get(QueueAttributeName.Policy.toString()));
+        Policy policy = extendPolicy ? Policy.fromJson(sqsAttrs.get(QueueAttributeName.Policy.toString())) : new Policy();
         policy.getStatements().add(new Statement(Effect.Allow)
                                            .withId("topic-subscription-" + snsTopicArn)
                                            .withPrincipals(Principal.AllUsers)
