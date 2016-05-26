28f4ff86e00b5f7cddffb01883e9ff794656c752
Andrew Shore <shorea@amazon.com>
2016-05-26 13:44:11 -0700

    Handle 200 response with partial payloads in S3

--- src/main/java/com/amazonaws/services/s3/internal/S3XmlResponseHandler.java
+++ src/main/java/com/amazonaws/services/s3/internal/S3XmlResponseHandler.java
@@ -20,8 +20,11 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonWebServiceResponse;
 import com.amazonaws.http.HttpResponse;
+import com.amazonaws.services.s3.Headers;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.transform.Unmarshaller;
 
 /**
@@ -59,12 +62,37 @@ public class S3XmlResponseHandler<T> extends AbstractS3ResponseHandler<T> {
 
         if (responseUnmarshaller != null) {
             log.trace("Beginning to parse service response XML");
-            T result = responseUnmarshaller.unmarshall(response.getContent());
+            T result = null;
+            try {
+                result = responseUnmarshaller.unmarshall(response.getContent());
+            } catch (AmazonClientException ace) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Received a successfull response. But failed to " +
+                              "parse the payload.");
+                }
+                throw newAmazonS3Exception(ace, response);
+            }
             log.trace("Done parsing service response XML");
             awsResponse.setResult(result);
         }
 
         return awsResponse;
+    }
+
+    private AmazonS3Exception newAmazonS3Exception(AmazonClientException ace,
+                                                   HttpResponse response) {
+        final AmazonS3Exception s3Exception = new AmazonS3Exception(
+                "Received a successfull response. But failed to " + "parse the payload.", ace);
+        s3Exception.setExtendedRequestId(getHeaderValue(Headers.EXTENDED_REQUEST_ID));
+        s3Exception.setRequestId(getHeaderValue(Headers.REQUEST_ID));
+        s3Exception.setStatusCode(response.getStatusCode());
+        s3Exception.setErrorCode("Invalid Response");
+        s3Exception.setServiceName(response.getRequest().getServiceName());
+        return s3Exception;
+    }
+
+    private String getHeaderValue(String header) {
+        return responseHeaders.get(header);
     }
 
     /**
