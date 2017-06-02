08c0e49847a16ec90ba1b08cd9355a5dcd39d68d
Soo Sung <soosung@amazon.com>
2017-06-02 14:02:34 -0700

    fixing SES sample code to use builder instead of deprecated constructors per customer feedback

--- src/samples/AmazonSimpleEmailService/AmazonSESSample.java
+++ src/samples/AmazonSimpleEmailService/AmazonSESSample.java
@@ -17,10 +17,11 @@ import java.io.IOException;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
 import com.amazonaws.services.simpleemail.model.Body;
 import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
@@ -85,17 +86,16 @@ public class AmazonSESSample {
                         "location (~/.aws/credentials), and is in valid format.",
                         e);
             }
-
+            
             // Instantiate an Amazon SES client, which will make the service call with the supplied AWS credentials.
-            AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(credentials);
-
             // Choose the AWS region of the Amazon SES endpoint you want to connect to. Note that your production
             // access status, sending limits, and Amazon SES identity-related settings are specific to a given
             // AWS region, so be sure to select an AWS region in which you set up Amazon SES. Here, we are using
-            // the US East (N. Virginia) region. Examples of other regions that Amazon SES supports are US_WEST_2
-            // and EU_WEST_1. For a complete list, see http://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html
-            Region REGION = Region.getRegion(Regions.US_EAST_1);
-            client.setRegion(REGION);
+            // the US West (Oregon) region. For a complete list, see http://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html
+            AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
+            		.withCredentials(new AWSStaticCredentialsProvider(credentials))
+            		.withRegion(Regions.US_WEST_2)
+            		.build();
 
             // Send the email.
             client.sendEmail(request);
