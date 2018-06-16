a3fff6cf89c9644f2c3cef06e6713e40257294aa
David Murray <dmurray@salesforce.com>
2018-06-15 18:56:17 -0700

    Renaming S3AbortableInputStream.readAllBytes() to avoid conflict with Java 9 InputStream.readAllBytes()

--- aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/internal/S3AbortableInputStream.java
+++ aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/internal/S3AbortableInputStream.java
@@ -172,7 +172,7 @@ public final class S3AbortableInputStream extends SdkFilterInputStream {
      */
     @Override
     public void close() throws IOException {
-        if (readAllBytes() || isAborted()) {
+        if (_readAllBytes() || isAborted()) {
             super.close();
         } else {
             LOG.warn(
@@ -218,7 +218,7 @@ public final class S3AbortableInputStream extends SdkFilterInputStream {
         }
     }
 
-    private boolean readAllBytes() {
+    private boolean _readAllBytes() {
         return bytesRead >= contentLength || eofReached;
     }
 }
