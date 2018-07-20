c66d7f87b368178bade1024c9b95f55a7655ffbc
Jan Steinke <jan.steinke@gmail.com>
2018-07-20 02:46:59 +0200

    Fix generic types on Return type for s3 waiters (#1639)
    * Fix generic types on Return type for s3 waiters
    
    * Add generics to S3 Acceptors

--- aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/waiters/AmazonS3Waiters.java
+++ aws-java-sdk-s3/src/main/java/com/amazonaws/services/s3/waiters/AmazonS3Waiters.java
@@ -50,12 +50,12 @@ public class AmazonS3Waiters {
      * entered the desired state or not, where polling criteria is bound by
      * either default polling strategy or custom polling strategy.
      */
-    public Waiter bucketNotExists() {
+    public Waiter<HeadBucketRequest> bucketNotExists() {
 
         return new WaiterBuilder<HeadBucketRequest, HeadBucketResult>()
                 .withSdkFunction(new HeadBucketFunction(client))
                 .withAcceptors(
-                        new HttpFailureStatusAcceptor(404, WaiterState.SUCCESS))
+                        new HttpFailureStatusAcceptor<HeadBucketResult>(404, WaiterState.SUCCESS))
                 .withDefaultPollingStrategy(
                         new PollingStrategy(new MaxAttemptsRetryStrategy(20),
                                 new FixedDelayStrategy(5)))
@@ -69,15 +69,15 @@ public class AmazonS3Waiters {
      * not, where polling criteria is bound by either default polling strategy
      * or custom polling strategy.
      */
-    public Waiter bucketExists() {
+    public Waiter<HeadBucketRequest> bucketExists() {
 
         return new WaiterBuilder<HeadBucketRequest, HeadBucketResult>()
                 .withSdkFunction(new HeadBucketFunction(client))
                 .withAcceptors(
-                        new HttpSuccessStatusAcceptor(WaiterState.SUCCESS),
-                        new HttpFailureStatusAcceptor(301, WaiterState.SUCCESS),
-                        new HttpFailureStatusAcceptor(403, WaiterState.SUCCESS),
-                        new HttpFailureStatusAcceptor(404, WaiterState.RETRY))
+                        new HttpSuccessStatusAcceptor<HeadBucketResult>(WaiterState.SUCCESS),
+                        new HttpFailureStatusAcceptor<HeadBucketResult>(301, WaiterState.SUCCESS),
+                        new HttpFailureStatusAcceptor<HeadBucketResult>(403, WaiterState.SUCCESS),
+                        new HttpFailureStatusAcceptor<HeadBucketResult>(404, WaiterState.RETRY))
                 .withDefaultPollingStrategy(
                         new PollingStrategy(new MaxAttemptsRetryStrategy(20),
                                 new FixedDelayStrategy(5)))
@@ -91,13 +91,13 @@ public class AmazonS3Waiters {
      * not, where polling criteria is bound by either default polling strategy
      * or custom polling strategy.
      */
-    public Waiter objectExists() {
+    public Waiter<GetObjectMetadataRequest> objectExists() {
 
         return new WaiterBuilder<GetObjectMetadataRequest, ObjectMetadata>()
                 .withSdkFunction(new HeadObjectFunction(client))
                 .withAcceptors(
-                        new HttpSuccessStatusAcceptor(WaiterState.SUCCESS),
-                        new HttpFailureStatusAcceptor(404, WaiterState.RETRY))
+                        new HttpSuccessStatusAcceptor<ObjectMetadata>(WaiterState.SUCCESS),
+                        new HttpFailureStatusAcceptor<ObjectMetadata>(404, WaiterState.RETRY))
                 .withDefaultPollingStrategy(
                         new PollingStrategy(new MaxAttemptsRetryStrategy(20),
                                 new FixedDelayStrategy(5)))
@@ -111,12 +111,12 @@ public class AmazonS3Waiters {
      * entered the desired state or not, where polling criteria is bound by
      * either default polling strategy or custom polling strategy.
      */
-    public Waiter objectNotExists() {
+    public Waiter<GetObjectMetadataRequest> objectNotExists() {
 
         return new WaiterBuilder<GetObjectMetadataRequest, ObjectMetadata>()
                 .withSdkFunction(new HeadObjectFunction(client))
                 .withAcceptors(
-                        new HttpFailureStatusAcceptor(404, WaiterState.SUCCESS))
+                        new HttpFailureStatusAcceptor<ObjectMetadata>(404, WaiterState.SUCCESS))
                 .withDefaultPollingStrategy(
                         new PollingStrategy(new MaxAttemptsRetryStrategy(20),
                                 new FixedDelayStrategy(5)))
